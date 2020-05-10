46e29cac6746f8d9d005125d01c61485265f4cef
josh <joshtrup0@gmail.com>
2020-05-09 18:44:46 -0700

    fixed stuff

--- app/src/main/java/com/example/cst_438_project3/DB/AppDatabase.java
+++ app/src/main/java/com/example/cst_438_project3/DB/AppDatabase.java
@@ -1,24 +1,15 @@
 package com.example.cst_438_project3.DB;
 
-
-
 import android.content.Context;
-
-
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
 
 import com.example.cst_438_project3.Objects.User;
-
 import com.example.cst_438_project3.Objects.Workout;
-import com.example.cst_438_project3.DB.WorkoutDAO;
-
-
-
 import com.example.cst_438_project3.Objects.WeightLog;
-import com.example.cst_438_project3.Objects.Workout;
+
 
 @Database(entities = {User.class, Workout.class, WeightLog.class}, version =1, exportSchema = false)
 
@@ -26,15 +17,16 @@ public abstract class AppDatabase extends RoomDatabase {
 
     public static AppDatabase instance;
 
+
     //daos
     public abstract UserDAO userDAO();
-    public abstract WorkoutDAO workoutDAO();
+    public abstract WorkoutDAO WorkoutDAO();
     public abstract WeightLogDAO weightLogDAO();
 
     //tables
 
     public static final String USER_TABLE ="userTable";
-    public static final String WORKOUT_TABLE ="workout";
+    public static final String WORKOUT_TABLE ="workoutTable";
     public static final String WEIGHT_LOG_TABLE ="weightLogTable";
 
 
@@ -46,7 +38,7 @@ public abstract class AppDatabase extends RoomDatabase {
         if (instance == null) {
             instance = Room.databaseBuilder(context.getApplicationContext(),
                     AppDatabase.class,
-                    "FitnessApp Database") // database name
+                    "Fit Lyfe Database") // database name
                     .allowMainThreadQueries()
                     .fallbackToDestructiveMigration()
                     .build();
--- app/src/main/java/com/example/cst_438_project3/DB/WorkoutDAO.java
+++ app/src/main/java/com/example/cst_438_project3/DB/WorkoutDAO.java
@@ -1,27 +1,3 @@
-
-
-// @Dao
-// public interface WorkoutDAO {
-
-//     @Insert
-//     public void insert(Workout workout);
-
-//     @Delete
-//     public void delete(Workout workout);
-
-//     @Update
-//     public void update(Workout workout);
-
-//     @Query("SELECT * FROM " + AppDatabase.WORKOUT_TABLE)
-//     List<Workout> getAllWorkouts();
-
-//     @Query("SELECT * FROM " + AppDatabase.WORKOUT_TABLE + " WHERE userID = :id")
-//     List<Workout> getusrWorkouts(String id) ;
-
-
-
-// }
-
 package com.example.cst_438_project3.DB;
 
 import androidx.room.Dao;
@@ -35,22 +11,21 @@ import com.example.cst_438_project3.Objects.Workout;
 import java.util.List;
 
 @Dao
-public interface WorkoutDAO {
+ public interface WorkoutDAO {
 
-    @Update
-    void update(Workout workout);
+     @Insert
+     public void insert(Workout workout);
 
-    @Insert
-    void insert(Workout workout);
+     @Delete
+     public void delete(Workout workout);
 
-    @Delete
-    void delete(Workout workout);
+     @Update
+     public void update(Workout workout);
 
-    @Query("select * from workoutTable")
-    List<Workout> getAllWorkouts();
+     @Query("SELECT * FROM " + AppDatabase.WORKOUT_TABLE)
+     List<Workout> getAllWorkouts();
 
-    //pull a workout from the DB by ID
-    @Query("select * from workoutTable where WorkoutID = :WorkoutID")
-    Workout getWorkoutByID(int WorkoutID);
-}
+     @Query("SELECT * FROM " + AppDatabase.WORKOUT_TABLE + " WHERE userID = :id")
+     List<Workout> getusrWorkouts(String id) ;
 
+ }
