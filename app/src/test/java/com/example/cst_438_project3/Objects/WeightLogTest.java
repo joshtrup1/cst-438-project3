ae817f390dd2a0e447851a9e8057ac5cdc6c51b8
EmmanuelC1 <emcastillo@csumb.edu>
2020-05-09 23:23:39 -0700

    added JUnit Tests

--- app/src/test/java/com/example/cst_438_project3/Objects/WeightLogTest.java
+++ app/src/test/java/com/example/cst_438_project3/Objects/WeightLogTest.java
@@ -1,7 +1,34 @@
 package com.example.cst_438_project3.Objects;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class WeightLogTest {
+    @Test
+    public void WeightLog() {
+        WeightLog w = new WeightLog();
+        assertNotNull(w);
+    }
 
+    @Test
+    public void setWeight() {
+        WeightLog w = new WeightLog();
+        w.setWeight(150);
+        assertTrue(w.getWeight() == 150);
+    }
+
+    @Test
+    public void setWeightLogID() {
+        WeightLog w = new WeightLog();
+        w.setWeightLogID(1);
+        assertTrue(w.getWeightLogID() == 1);
+    }
+
+    @Test
+    public void setUserID() {
+        WeightLog w = new WeightLog();
+        w.setUserID(1);
+        assertTrue(w.getUserID() == 1);
+    }
 }
